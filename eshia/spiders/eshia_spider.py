c5a27e94e546c9a870e571012d15deae6e4f5516
taaahaaa <taaahaaa75@gmail.com>
2022-03-03 11:12:06 +0330

    Replace comma with nothing

--- eshia/spiders/eshia_spider.py
+++ eshia/spiders/eshia_spider.py
@@ -64,11 +64,11 @@ class EshiaSpider(scrapy.Spider):
         content_table = response.css("#BooksList > tbody > tr").getall()
         for i in range(0, len(content_table)):
             content = {}
-            content["Category"] = unquote(category)
-            content["Subcategory"] = unquote(sub_category)
-            content["Book name"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookName-sub > a::text').get()
-            content["Book url"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookName-sub > a::attr(href)').get()
-            content["Author name"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookAuthor-sub > a::text').get()
-            content["Author url"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookAuthor-sub > a::attr(href)').get()
+            content["Category"] = unquote(category).replace(",", "")
+            content["Subcategory"] = unquote(sub_category).replace(",", "")
+            content["Book name"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookName-sub > a::text').get().replace(",", "")
+            content["Book url"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookName-sub > a::attr(href)').get().replace(",", "")
+            content["Author name"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookAuthor-sub > a::text').get().replace(",", "")
+            content["Author url"] = response.css(f'#BooksList > tbody > tr:nth-child({i+1}) > td.BookAuthor-sub > a::attr(href)').get().replace(",", "")
             
             self.data.append(content)
